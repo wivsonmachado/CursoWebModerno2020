2b449ec639a56e7be5c06d322d9092a1db121a66
Wivson Machado <wivsonfrota@gmail.com>
2020-12-10 13:34:11 -0300

    Como o JavaScript é organizado

---
+++ Seção 3 - Fundamendots/organizacao.js
@@ -0,0 +1,17 @@
+/*
+O código JS pode ser organizado em uma sentença de código ou em um bloco de código. */
+
+console.log("Sentença de código") // Isso é uma sentença de código
+
+{
+    // O bloco de código é reprensentado entre chaves
+
+    { // É possivel ter um bloco dentro do outro
+
+        console.log("Olá"); // O papel do bloco é agrupar sentenças de código
+        console.log('Mundo!') // Padrão do curso (aspas simples e sem ponto-virgula)
+
+    }
+
+}
+
